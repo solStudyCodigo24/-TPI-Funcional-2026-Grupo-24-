0c1e2a004a281fdd761338c3fb2464baa61b835c
lautaDwDev <lautadw@gmail.com>
2026-06-22 05:33:38 -0300

    correccion req 2 y aux core.lisp

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -16,3 +16,28 @@
 	    (t (list color-actual 'accion-por-defecto))
 	)
 )
+
+;;====================================================================
+;; FUNCIÓN: Sumar-ciclos
+;; NATURALEZA: Pura.
+;; ESTRATEGIA: Devuelve una suma de la cant. de segundos de duración del semf. 
+;; en rojo 90 seg., amarillo 6 seg. , verde 120 seg. , las tres veces que aparecen
+;; 3 es porque se agrega la intermitencia, del punto iterativo.
+;; IMPACTO: No destructiva (Auxiliar para funcion timer y recomendacion-ciclo-ing)
+;;====================================================================
+(defun sumar-ciclos ()
+  (+ 90 3 6 3 120 3))
+
+;;====================================================================
+;; FUNCIÓN: timer
+;; NATURALEZA: Pura (dado un timestamp, siempre retorna el mismo color)
+;; ESTRATEGIA: Selectiva (uso de cond y operador matemático rem para obtener el resto)
+;; IMPACTO: No destructiva (solo calcula y devuelve resultados)
+;;====================================================================
+(defun timer (tiempo-unix)
+  (cond
+    ((< tiempo-unix 0)  "El tiempo Unix no puede ser negativo")
+    ((< (rem tiempo-unix (sumar-ciclos)) 90)  'en-rojo)
+    ((< (rem tiempo-unix (sumar-ciclos)) 96)  'en-amarillo)
+    (t  'en-verde)))
+
